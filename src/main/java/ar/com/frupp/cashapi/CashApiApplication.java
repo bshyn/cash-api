a722cffe554e846e2432a15a02e8d2c8aaa9f237
Franco Rupp <frupp@quaresitsolutions.com>
2021-06-14 18:20:02 -0300

    first commit/spring initializr

---
+++ src/main/java/ar/com/frupp/cashapi/CashApiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.frupp.cashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/frupp/cashapi/CashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.frupp.cashapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
